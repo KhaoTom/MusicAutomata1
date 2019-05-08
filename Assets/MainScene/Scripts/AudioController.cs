03984e33b1c31695a6c0d00834b13018603291d7
Edwin DeNicholas <disruption999@hotmail.com>
2019-05-08 00:09:03 -0700

    basic attempt at smoothing volume changes

--- Assets/MainScene/Scripts/AudioController.cs
+++ Assets/MainScene/Scripts/AudioController.cs
@@ -6,6 +6,37 @@ public class AudioController : MonoBehaviour
 {
     [SerializeField]
     private AudioSource[] audioSources = new AudioSource[0];
+    private float[] targetVolumes;
+    private int numSources;
+
+    private void Start()
+    {
+        numSources = audioSources.Length;
+        targetVolumes = new float[numSources];
+        for (int i = 0; i < numSources; i++)
+        {
+            targetVolumes[i] = 0.0f;
+        }
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < numSources; i++)
+        {
+            AudioSource src = audioSources[i];
+            float sv = src.volume;
+            float tv = targetVolumes[i];
+            
+            if (sv < tv)
+            {
+                src.volume += (tv - sv) * 0.4f;
+            }
+            else
+            {
+                src.volume -= (sv - tv) * 0.4f;
+            }
+        }
+    }
 
     public void PlayAll()
     {
@@ -30,6 +61,6 @@ public class AudioController : MonoBehaviour
 
     public void SetVolume(int id, float vol)
     {
-        audioSources[id].volume = vol;
+        targetVolumes[id] = vol;
     }
 }
